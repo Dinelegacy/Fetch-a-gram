c317c5fc8450a090594dbacd0ee4c58a49edba8b
Yordanos A. <107444432+Yordiemma@users.noreply.github.com>
2025-10-28 23:43:45 +0100

    Save my header and dark mode changes before merging Anna's layout

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import './style.css'
+// header Yordanos
+
+// Select the toggle button
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn?.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  const dark = document.body.classList.contains("dark-mode");
+  toggleBtn.textContent = dark ? "☀️ Light Mode" : "🌙 Dark Mode";
+});
+
+
 
