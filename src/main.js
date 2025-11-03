ae3da7c89552843a289354e5a7907633987dcb37
Yordanos A. <107444432+Yordiemma@users.noreply.github.com>
2025-11-03 10:51:55 +0100

    Cleaned file to start for learing

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
 
 import './style.css';
-import setupHeader from './header.js';
-import setupFeed from './feed.js';
 
-import setupPopup from './popup.js';
-const openPopup = setupPopup();
-
-setupHeader();
-setupFeed(openPopup);
